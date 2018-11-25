b9ba4f78a0b3b437c2cfe3d6c0c7501eed03e422
Angela Mitchell <anglarenmit@gmail.com>
2018-11-24 21:27:12 -0600

    Prep Started for 3.8 Prep Work

--- src/main/java/com/amitchell05/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/amitchell05/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package com.amitchell05.cheesemvc.controllers;
 
+import com.amitchell05.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
     static HashMap<String, String> cheeses = new HashMap<>();
+//    static ArrayList<Object> cheeses = new ArrayList<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -31,24 +34,26 @@ public class CheeseController {
         return "cheese/add";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.POST)
+    @RequestMapping(value ="add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
         cheeses.put(cheeseName, cheeseDescription);
+//        Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
+//        cheeses.add(newCheese);
         return "redirect:";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", cheeses.keySet());
         model.addAttribute("title", "Remove Cheese");
         return "cheese/remove";
     }
 
+
     @RequestMapping(value = "remove", method = RequestMethod.POST)
     public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheese) {
-        // Optional check: Entries are out of order. Is there a way to sort them?
-        for (String name : cheese) {
-            cheeses.remove(name);
+        for (String aCheese : cheese) {
+            cheeses.remove(aCheese);
         }
         return "redirect:";
     }
---
+++ src/main/java/com/amitchell05/cheesemvc/models/Cheese.java
@@ -0,0 +1,29 @@
+package com.amitchell05.cheesemvc.models;
+
+public class Cheese {
+    private String name;
+    private String description;
+
+    public Cheese(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+
+
+}
